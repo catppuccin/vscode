de73bd9d42a6b7449545f754f6f69afc9e427432
winston <hey@winston.sh>
2023-01-29 02:07:44 +0100

    feat: add support for VSCode web

--- build.js
+++ build.js
@@ -11,3 +11,16 @@ require('esbuild').build({
     platform: "node",
     minify: true,
 }).catch(() => process.exit(1))
+
+require('esbuild').build({
+    entryPoints: ['src/browser.ts'],
+    bundle: true,
+    sourcemap: "DEBUG" in process.env,
+    outfile: 'dist/browser.js',
+    external: [
+        "vscode"
+    ],
+    format: "cjs",
+    platform: "node",
+    minify: true,
+}).catch(() => process.exit(1))
