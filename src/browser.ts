111eaff814704087993f8bc79e88931f4c1fedc6
winston <hey@winston.sh>
2023-01-29 02:15:34 +0100

    feat: add support for vscode web (#85)
    * chore: update dependencies
    
    * feat: add support for VSCode web

---
+++ src/browser.ts
@@ -0,0 +1,14 @@
+import { workspace, ConfigurationChangeEvent, window } from "vscode";
+
+export const activate = () => {
+  // regenerate the theme files when the config changes
+  workspace.onDidChangeConfiguration((event: ConfigurationChangeEvent) => {
+    if (event.affectsConfiguration("catppuccin")) {
+      window.showInformationMessage(
+        "VSCode Web doesn't support advanced Catppuccin options at the moment."
+      );
+    }
+  });
+};
+
+export const deactivate = () => {};
