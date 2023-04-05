19251b6ef6e3bcbd28eede1b21a14588c5062637
winston <hey@winston.sh>
2023-04-05 04:43:08 +0200

    build: simplify esbuild

--- build.js
+++ build.js
@@ -1,26 +1,15 @@
-/** @type {import('esbuild').BuildOptions} */
-require('esbuild').build({
-    entryPoints: ['src/main.ts'],
-    bundle: true,
-    sourcemap: "DEBUG" in process.env,
-    outfile: 'dist/main.js',
-    external: [
-        "vscode"
-    ],
-    format: "cjs",
-    platform: "node",
-    minify: true,
-}).catch(() => process.exit(1))
-
-require('esbuild').build({
-    entryPoints: ['src/browser.ts'],
-    bundle: true,
-    sourcemap: "DEBUG" in process.env,
-    outfile: 'dist/browser.js',
-    external: [
-        "vscode"
-    ],
-    format: "cjs",
-    platform: "node",
-    minify: true,
-}).catch(() => process.exit(1))
+["main", "browser"].forEach((file) => {
+  /** @type {import('esbuild').BuildOptions} */
+  require("esbuild")
+    .build({
+      entryPoints: [`src/${file}.ts`],
+      bundle: true,
+      sourcemap: "DEBUG" in process.env,
+      outfile: `dist/${file}.js`,
+      external: ["vscode"],
+      format: "cjs",
+      platform: "node",
+      minify: true,
+    })
+    .catch(() => process.exit(1));
+});
