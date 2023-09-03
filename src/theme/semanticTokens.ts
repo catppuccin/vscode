11519989c22ba0b4e8aeb6d34d7c264cf86789fe
winston <hey@winston.sh>
2023-09-03 16:58:54 +0200

    fix(syntax): add jsx/tsx semantic highlights for const

--- src/theme/semanticTokens.ts
+++ src/theme/semanticTokens.ts
@@ -4,18 +4,30 @@ export const getSemanticTokens = (context: ThemeContext): SemanticTokens => {
   const { palette } = context;
 
   return {
-    "class:python": { foreground: palette.yellow },
-    "class.builtin:python": { foreground: palette.mauve },
     enumMember: { foreground: palette.teal },
     selfKeyword: { foreground: palette.red },
     "variable.defaultLibrary": { foreground: palette.maroon },
-    // ignore `consts` being peach in JS & TS
+
+    // Python types
+    "class:python": { foreground: palette.yellow },
+    "class.builtin:python": { foreground: palette.mauve },
+
+    // ignore `const`s being peach in JS & TS
     "variable.readonly:javascript": { foreground: palette.text },
     "variable.readonly:typescript": { foreground: palette.text },
     "property.readonly:javascript": { foreground: palette.text },
     "property.readonly:typescript": { foreground: palette.text },
+    // ditto for React
+    "variable.readonly:javascriptreact": { foreground: palette.text },
+    "variable.readonly:typescriptreact": { foreground: palette.text },
+    "property.readonly:javascriptreact": { foreground: palette.text },
+    "property.readonly:typescriptreact": { foreground: palette.text },
+
+    // TOML syntax
     tomlArrayKey: { foreground: palette.blue, fontStyle: "" },
     tomlTableKey: { foreground: palette.blue, fontStyle: "" },
+
+    // Rust attributes
     "builtinAttribute.attribute.library:rust": { foreground: palette.blue },
     "generic.attribute:rust": { foreground: palette.text },
 
