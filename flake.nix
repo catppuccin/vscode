ffe16251e564c388389e0f527a37e78186ffb8c7
winston <hey@winston.sh>
2023-09-04 03:54:32 +0200

    fix(nix): use callPackage

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {nixpkgs, ...} @ inputs: let
+  outputs = {nixpkgs, ...}: let
     systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
   in {
     packages = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in rec {
-      catppuccin-vsc = import ./. {inherit pkgs;};
+      catppuccin-vsc = pkgs.callPackage ./. {};
       default = catppuccin-vsc;
     });
   };
