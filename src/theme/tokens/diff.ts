f148b6cb1fef18d759c13ae03b226d8a5bdbb89b
Krypton <coderkrypton@gmail.com>
2023-09-21 15:04:11 +0200

    feat(syntax): Diff highlighting (#182)

---
+++ src/theme/tokens/diff.ts
@@ -0,0 +1,47 @@
+import type { TextmateColors, ThemeContext } from "../../types";
+
+const tokens = (context: ThemeContext): TextmateColors => {
+  const { palette } = context;
+
+  return [
+    {
+      name: "Markup Diff",
+      scope: "markup.changed.diff",
+      settings: {
+        foreground: palette.peach,
+        fontStyle: "",
+      },
+    },
+    {
+      name: "Diff",
+      scope: [
+        "meta.diff.header.from-file",
+        "meta.diff.header.to-file",
+        "punctuation.definition.from-file.diff",
+        "punctuation.definition.to-file.diff",
+      ],
+      settings: {
+        foreground: palette.blue,
+        fontStyle: "",
+      },
+    },
+    {
+      name: "Diff Inserted",
+      scope: "markup.inserted.diff",
+      settings: {
+        foreground: palette.green,
+        fontStyle: "",
+      },
+    },
+    {
+      name: "Diff Deleted",
+      scope: "markup.deleted.diff",
+      settings: {
+        foreground: palette.red,
+        fontStyle: "",
+      },
+    },
+  ];
+};
+
+export default tokens;
