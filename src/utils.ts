5c3be7a7cd4c8aedfaea49db01f05985d1b7f74d
winston <hey@winston.sh>
2023-10-30 21:46:55 +0100

    refactor: get rid of utils class (#209)

--- src/utils.ts
+++ src/utils.ts
@@ -27,138 +27,143 @@ export enum UpdateTrigger {
   FRESH_INSTALL = "Update detected",
 }
 
-class Utils {
-  private promptToReload = (trigger: UpdateTrigger) => {
-    const msg = `Catppuccin: ${trigger} - Reload required.`;
-    const action = "Reload window";
-    window.showInformationMessage(msg, action).then((selectedAction) => {
-      if (selectedAction === action) {
-        commands.executeCommand("workbench.action.reloadWindow");
-      }
-    });
-  };
-  private writeThemeFile = async (uri: Uri, data: any): Promise<void> => {
-    return workspace.fs
-      .writeFile(uri, Buffer.from(JSON.stringify(data, null, 2)))
-      .then(
-        () => {},
-        (err) => {
-          window.showErrorMessage(err.message);
-        },
-      );
-  };
-  private fileExists = async (uri: Uri): Promise<boolean> => {
-    return workspace.fs.stat(uri).then(
+type Entry<T> = { [K in keyof T]: [K, T[K]] }[keyof T];
+
+const filterObject = <T extends object>(
+  obj: T,
+  fn: (entry: Entry<T>, i: number, arr: Entry<T>[]) => boolean,
+) => {
+  return Object.fromEntries(
+    (Object.entries(obj) as Entry<T>[]).filter(fn),
+  ) as Partial<T>;
+};
+
+export const promptToReload = (trigger: UpdateTrigger) => {
+  const msg = `Catppuccin: ${trigger} - Reload required.`;
+  const action = "Reload window";
+  window.showInformationMessage(msg, action).then((selectedAction) => {
+    if (selectedAction === action) {
+      commands.executeCommand("workbench.action.reloadWindow");
+    }
+  });
+};
+
+const writeThemeFile = async (uri: Uri, data: any): Promise<void> => {
+  return workspace.fs
+    .writeFile(uri, Buffer.from(JSON.stringify(data, null, 2)))
+    .then(
+      () => {},
+      (err) => {
+        window.showErrorMessage(err.message);
+      },
+    );
+};
+
+const fileExists = async (uri: Uri): Promise<boolean> => {
+  return workspace.fs.stat(uri).then(
+    () => true,
+    () => false,
+  );
+};
+
+// TODO: listen to this to determine if a user is using Nix, redirect to README
+export const isMutable = async (uri: Uri): Promise<boolean> => {
+  return workspace.fs.stat(uri).then(
+    (stat) => stat.permissions !== FilePermission.Readonly,
+    (err) => err,
+  );
+};
+
+export const isFreshInstall = async (
+  ctx: ExtensionContext,
+): Promise<boolean | "error"> => {
+  console.log("Checking if catppuccin is installed for the first time.");
+  const flagUri = Uri.file(ctx.asAbsolutePath("themes/.flag"));
+  if (await fileExists(flagUri)) {
+    console.log("Catppuccin has been installed before.");
+    return false;
+  } else {
+    console.log("Catppuccin is installed for the first time!");
+    return workspace.fs.writeFile(flagUri, Buffer.from("")).then(
       () => true,
-      () => false,
+      () => "error",
     );
+  }
+};
+
+export const isDefaultConfig = (): boolean => {
+  console.log("Checking if catppuccin is using default config.");
+  const state =
+    JSON.stringify(getConfiguration()) === JSON.stringify(defaultOptions);
+  console.log(`Catppuccin is using ${state ? "default" : "custom"} config.`);
+
+  return state;
+};
+
+export const getConfiguration = (): ThemeOptions => {
+  const conf = workspace.getConfiguration("catppuccin");
+  const options = {
+    accent: conf.get<CatppuccinAccent>("accentColor"),
+    boldKeywords: conf.get<boolean>("boldKeywords"),
+    italicKeywords: conf.get<boolean>("italicKeywords"),
+    italicComments: conf.get<boolean>("italicComments"),
+    colorOverrides: conf.get<ColorOverrides>("colorOverrides"),
+    workbenchMode: conf.get<CatppuccinWorkbenchMode>("workbenchMode"),
+    bracketMode: conf.get<CatppuccinBracketMode>("bracketMode"),
+    extraBordersEnabled: conf.get<boolean>("extraBordersEnabled"),
+    customUIColors: conf.get<CustomUIColors>("customUIColors"),
   };
-  isMutable = async (uri: Uri): Promise<boolean> => {
-    return workspace.fs.stat(uri).then(
-      (stat) => stat.permissions !== FilePermission.Readonly,
-      (err) => err,
-    );
+  return {
+    ...defaultOptions,
+    ...filterObject(options, ([, value]) => value !== undefined),
   };
-  isFreshInstall = async (
-    ctx: ExtensionContext,
-  ): Promise<boolean | "error"> => {
-    console.log("Checking if catppuccin is installed for the first time.");
-    const flagUri = Uri.file(ctx.asAbsolutePath("themes/.flag"));
-    if (await this.fileExists(flagUri)) {
-      console.log("Catppuccin has been installed before.");
-      return false;
-    } else {
-      console.log("Catppuccin is installed for the first time!");
-      return workspace.fs.writeFile(flagUri, Buffer.from("")).then(
-        () => true,
-        () => "error",
-      );
-    }
+};
+
+export const updateThemes = async (
+  options: ThemeOptions,
+  paths: ThemePaths,
+  trigger: UpdateTrigger,
+) => {
+  const flavors = Object.keys(variants) as CatppuccinFlavor[];
+
+  const promises = flavors.map(async (flavor): Promise<void> => {
+    const theme = compileTheme(flavor, options);
+    return writeThemeFile(paths[flavor], theme);
+  });
+
+  Promise.all(promises).then(() => {
+    promptToReload(trigger);
+  });
+};
+
+export const syncToIconPack = () => {
+  const id = "catppuccin.catppuccin-vsc-icons";
+  // bail if the icon pack isn't installed
+  if (extensions.getExtension(id) === undefined) return;
+
+  // mapping the Catppuccin Theme names to the icon pack names
+  const uiThemesToIconThemes = {
+    "Catppuccin Latte": "catppuccin-latte",
+    "Catppuccin Frappé": "catppuccin-frappe",
+    "Catppuccin Macchiato": "catppuccin-macchiato",
+    "Catppuccin Mocha": "catppuccin-mocha",
   };
-  isDefaultConfig = (): boolean => {
-    console.log("Checking if catppuccin is using default config.");
-    const state =
-      JSON.stringify(this.getConfiguration()) ===
-      JSON.stringify(defaultOptions);
-    console.log(`Catppuccin is using ${state ? "default" : "custom"} config.`);
 
-    return state;
-  };
-  getConfiguration = (): ThemeOptions => {
-    const conf = workspace.getConfiguration("catppuccin");
-    return {
-      accent:
-        conf.get<CatppuccinAccent>("accentColor") ?? defaultOptions.accent,
-      boldKeywords:
-        conf.get<boolean>("boldKeywords") ?? defaultOptions.boldKeywords,
-      italicKeywords:
-        conf.get<boolean>("italicKeywords") ?? defaultOptions.italicKeywords,
-      italicComments:
-        conf.get<boolean>("italicComments") ?? defaultOptions.italicComments,
-      colorOverrides:
-        conf.get<ColorOverrides>("colorOverrides") ??
-        defaultOptions.colorOverrides,
-      workbenchMode:
-        conf.get<CatppuccinWorkbenchMode>("workbenchMode") ??
-        defaultOptions.workbenchMode,
-      bracketMode:
-        conf.get<CatppuccinBracketMode>("bracketMode") ??
-        defaultOptions.bracketMode,
-      extraBordersEnabled:
-        conf.get<boolean>("extraBordersEnabled") ??
-        defaultOptions.extraBordersEnabled,
-      customUIColors:
-        conf.get<CustomUIColors>("customUIColors") ??
-        defaultOptions.customUIColors,
-    };
-  };
-  updateThemes = async (
-    options: ThemeOptions,
-    paths: ThemePaths,
-    trigger: UpdateTrigger,
-  ) => {
-    const flavors = Object.keys(variants) as CatppuccinFlavor[];
+  // check if the current editor theme is a Catppuccin theme
+  const uiTheme =
+    workspace.getConfiguration("workbench").get<string>("colorTheme") ?? "";
+  const ctpThemeActive = Object.keys(uiThemesToIconThemes).includes(uiTheme);
 
-    const promises = flavors.map(async (flavor): Promise<void> => {
-      const theme = compileTheme(flavor, options);
-      return this.writeThemeFile(paths[flavor], theme);
-    });
+  // and only sync to a Catppuccin icon flavor if the user's currently using Catppuccin for icons
+  const ctpIconsActive = Object.values(uiThemesToIconThemes).includes(
+    workspace.getConfiguration("workbench").get<string>("iconTheme") ?? "",
+  );
 
-    Promise.all(promises).then(() => {
-      this.promptToReload(trigger);
-    });
-  };
-  syncToIconPack = () => {
-    const id = "catppuccin.catppuccin-vsc-icons";
-    // bail if the icon pack isn't installed
-    if (extensions.getExtension(id) === undefined) return;
-
-    // mapping the Catppuccin Theme names to the icon pack names
-    const uiThemesToIconThemes = {
-      "Catppuccin Latte": "catppuccin-latte",
-      "Catppuccin Frappé": "catppuccin-frappe",
-      "Catppuccin Macchiato": "catppuccin-macchiato",
-      "Catppuccin Mocha": "catppuccin-mocha",
-    };
-
-    // check if the current editor theme is a Catppuccin theme
-    const uiTheme =
-      workspace.getConfiguration("workbench").get<string>("colorTheme") ?? "";
-    const ctpThemeActive = Object.keys(uiThemesToIconThemes).includes(uiTheme);
-
-    // and only sync to a Catppuccin icon flavor if the user's currently using Catppuccin for icons
-    const ctpIconsActive = Object.values(uiThemesToIconThemes).includes(
-      workspace.getConfiguration("workbench").get<string>("iconTheme") ?? "",
-    );
-
-    if (ctpThemeActive && ctpIconsActive) {
-      const iconTheme =
-        uiThemesToIconThemes[uiTheme as keyof typeof uiThemesToIconThemes];
-      workspace
-        .getConfiguration("workbench")
-        .update("iconTheme", iconTheme, ConfigurationTarget.Global);
-    }
-  };
-}
-
-export default new Utils();
+  if (ctpThemeActive && ctpIconsActive) {
+    const iconTheme =
+      uiThemesToIconThemes[uiTheme as keyof typeof uiThemesToIconThemes];
+    workspace
+      .getConfiguration("workbench")
+      .update("iconTheme", iconTheme, ConfigurationTarget.Global);
+  }
+};
