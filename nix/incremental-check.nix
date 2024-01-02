226176d68f1cc071758fb40bf7b495bfabab11ff
winston <hey@winston.sh>
2024-01-02 08:59:53 +0100

    chore: add comment to incremental build check

--- nix/incremental-check.nix
+++ nix/incremental-check.nix
@@ -1,3 +1,8 @@
+# If incremental builds work, this derivation should be the base for the
+# customized derivation. To check that incremental builds work, run:
+# > nix-build ./incremental-check.nix -A default
+# > nix-build ./incremental-check.nix -A customized
+# The rebuild should take less than 5 seconds for the customized derivation.
 {pkgs ? import <nixpkgs> {}}: let
   default = pkgs.callPackage ./. {};
 in {
