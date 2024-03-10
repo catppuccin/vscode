c6288c44f2c17619b1eda3def6ff829b72368ff7
winston <hey@winston.sh>
2024-03-10 13:21:21 +0100

    fix: adjust for new esm module

--- packages/catppuccin-vscode/build.ts
+++ packages/catppuccin-vscode/build.ts
@@ -9,27 +9,27 @@ import latte from "catppuccin-vsc/themes/latte.json" with { type: "json" };
 
 const root = new URL("themes", import.meta.url).pathname;
 
-async () => {
-  // create the dir
-  await mkdir(root, { recursive: true });
-  // empty the dir
-  const files = await readdir(root);
-  await Promise.all(files.map((file) => unlink(join(root, file))));
-  // write the files
-  await Promise.all([
-    writeFile(join(root, "mocha.json"), JSON.stringify(mocha)),
-    writeFile(join(root, "macchiato.json"), JSON.stringify(macchiato)),
-    writeFile(join(root, "frappe.json"), JSON.stringify(frappe)),
-    writeFile(join(root, "latte.json"), JSON.stringify(latte)),
-  ]);
+// create the dir
+await mkdir(root, { recursive: true });
+// empty the dir
+const files = await readdir(root);
+await Promise.all(files.map((file) => unlink(join(root, file))));
+// write the files
+await Promise.all([
+  writeFile(join(root, "mocha.json"), JSON.stringify(mocha)),
+  writeFile(join(root, "macchiato.json"), JSON.stringify(macchiato)),
+  writeFile(join(root, "frappe.json"), JSON.stringify(frappe)),
+  writeFile(join(root, "latte.json"), JSON.stringify(latte)),
+]);
 
-  await build({
-    clean: true,
-    entryPoints: ["src/index.ts"],
-    format: ["cjs", "esm"],
-    dts: true,
-    sourcemap: true,
-    minify: true,
-    target: "node16",
-  });
-};
+await build({
+  clean: true,
+  entryPoints: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: {
+    resolve: true,
+  },
+  sourcemap: true,
+  minify: true,
+  target: "node16",
+});
--- packages/catppuccin-vscode/src/index.ts
+++ packages/catppuccin-vscode/src/index.ts
@@ -1,10 +1,6 @@
 import { FlavorName } from "@catppuccin/palette";
-import {
-  compileTheme,
-  defaultOptions,
-} from "catppuccin-vsc/src/hooks/generateThemes";
+import { compileTheme, defaultOptions } from "catppuccin-vsc/src/theme";
 import { ThemeOptions } from "catppuccin-vsc/src/types";
-export { ThemeOptions } from "catppuccin-vsc/src/types";
 
 export const compile = (
   flavor: FlavorName,
