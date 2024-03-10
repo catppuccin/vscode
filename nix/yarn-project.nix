c97e3cd7fabb32cd51e3f0736403f7598e479e47
winston <hey@winston.sh>
2024-03-10 22:11:05 +0100

    chore: bump dependencies (#340)

--- nix/yarn-project.nix
+++ nix/yarn-project.nix
@@ -55,7 +55,7 @@ let
       rm $out/.gitignore
     '';
     outputHashMode = "recursive";
-    outputHash = "sha512-1nPLkC50PWtZZsp9eVO1T5iipwLrM74RqubgYEGUOrWe1v7zxmRs/EZli/U63gG59k10NmnkECP/p+LYOjrdTw==";
+    outputHash = "sha512-OSnWtpTd6PtVvHw6ITOIL9nX9K5e/8w9R/OM+QvZYv/lh9VIM6iUNOMOkM0suARJDJ4MB83sGb4R8qBlD06uWA==";
   };
 
   # Main project derivation.
