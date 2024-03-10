60558b47ff4bcf85e71f94d2213047b1a149caf9
winston <hey@winston.sh>
2024-03-10 15:18:27 +0100

    fix: shim theme name for shiki

--- packages/catppuccin-vscode/src/index.ts
+++ packages/catppuccin-vscode/src/index.ts
@@ -1,6 +1,25 @@
-export { default as latte } from "catppuccin-vsc/themes/latte.json" with { type: "json" };
-export { default as frappe } from "catppuccin-vsc/themes/frappe.json" with { type: "json" };
-export { default as macchiato } from "catppuccin-vsc/themes/macchiato.json" with { type: "json" };
-export { default as mocha } from "catppuccin-vsc/themes/mocha.json" with { type: "json" };
+import latteJson from "catppuccin-vsc/themes/latte.json" with { type: "json" };
+import frappeJson from "catppuccin-vsc/themes/frappe.json" with { type: "json" };
+import macchiatoJson from "catppuccin-vsc/themes/macchiato.json" with { type: "json" };
+import mochaJson from "catppuccin-vsc/themes/mocha.json" with { type: "json" };
 
 export { compile } from "./compile.js";
+
+// shim the name for the Shiki theme, as the name is used for the className
+
+export const latte = {
+  ...latteJson,
+  name: "catppuccin-latte",
+};
+export const frappe = {
+  ...frappeJson,
+  name: "catppuccin-frappe",
+};
+export const macchiato = {
+  ...macchiatoJson,
+  name: "catppuccin-macchiato",
+};
+export const mocha = {
+  ...mochaJson,
+  name: "catppuccin-mocha",
+};
