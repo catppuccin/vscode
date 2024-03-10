591fb5b5d4dd741c55ab1041cd62ee550575b493
winston <hey@winston.sh>
2024-03-10 21:35:52 +0100

    chore: bump dependencies

--- packages/catppuccin-vsc-storybook/stories/Syntax.tsx
+++ packages/catppuccin-vsc-storybook/stories/Syntax.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { getHighlighter, bundledLanguages, type ThemeInput } from "shikiji";
+import { getHighlighter, bundledLanguages, type ThemeInput } from "shiki";
 import "@catppuccin/palette/css/catppuccin.css";
 import mocha from "catppuccin-vsc/themes/mocha.json";
 import macchiato from "catppuccin-vsc/themes/macchiato.json";
 import frappe from "catppuccin-vsc/themes/frappe.json";
 import latte from "catppuccin-vsc/themes/latte.json";
 
-const themes = [mocha, macchiato, frappe, latte].map((theme, i) => {
+const themes = [mocha, macchiato, frappe, latte].map((theme, index) => {
   return {
     ...theme,
-    name: ["mocha", "macchiato", "frappe", "latte"][i],
+    name: ["mocha", "macchiato", "frappe", "latte"][index],
   } as unknown as ThemeInput;
 });
 const shiki = getHighlighter({
@@ -27,19 +27,17 @@ export const Syntax = ({
   const [rendered, setRendered] = useState<string>("");
   useEffect(() => {
     (async () => {
+      const highlighted = await shiki;
       setRendered(
-        (await shiki).codeToHtml(
-          await fetch(code).then((data) => data.text()),
-          {
-            lang: language,
-            themes: {
-              light: "latte",
-              frappe: "frappe",
-              macchiato: "macchiato",
-              dark: "mocha",
-            },
+        highlighted.codeToHtml(await fetch(code).then((data) => data.text()), {
+          lang: language,
+          themes: {
+            light: "latte",
+            frappe: "frappe",
+            macchiato: "macchiato",
+            dark: "mocha",
           },
-        ),
+        }),
       );
     })();
   }, []);
