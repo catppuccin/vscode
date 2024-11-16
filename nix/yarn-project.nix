aed20c6dacd295edc901935fb987e81a983152d0
sgoudham <sgoudham@gmail.com>
2024-11-16 19:07:25 +0000

    chore: update types

--- nix/yarn-project.nix
+++ nix/yarn-project.nix
@@ -55,7 +55,7 @@ let
       rm $out/.gitignore
     '';
     outputHashMode = "recursive";
-    outputHash = "sha512-hdP43ZJVfjv5O+wLPiogOHmOLm25kx2P0vVUVRm210yDi5YhSLnVtyzDJDPSRwunrHEUo2jzEs7327vePbuwiw==";
+    outputHash = "sha512-yATp5SuF4aPnCV/WBUimCAeotQNLc5Ksi0Aj5G6A3Q+9EOjTQtVaOuck1J9uV6DN4MMkPB6n1pCS5sp1aTYANA==";
   };
 
   # Main project derivation.
--- packages/catppuccin-vsc-typegen/update.ts
+++ packages/catppuccin-vsc-typegen/update.ts
@@ -2,51 +2,58 @@
 import path from "node:path";
 import { writeFileSync } from "node:fs";
 import { compile, JSONSchema } from "json-schema-to-typescript";
-const vscodeSchemasRoot =
-  "https://raw.githubusercontent.com/ota-meshi/extract-vscode-schemas/main/resources/vscode/schemas/";
+// const vscodeSchemasRoot = "https://raw.githubusercontent.com/ota-meshi/extract-vscode-schemas/main/resources/vscode/schemas/";
 
 const bannerComment = `/**
  * This file was automatically generated.
  * DO NOT MODIFY IT BY HAND.
- * Instead, run \`yarn run updateVSCtypes\` to regenerate this file.
+ * Instead, run \`yarn run typegen:update\` to regenerate this file.
  */`;
 
 const mappings = [
-  {
-    schema: vscodeSchemasRoot + "token-styling.json",
-    name: "SemanticTokens",
-    fname: "token-styling.d.ts",
-    kind: "jsonschema",
-  },
-  {
-    schema: vscodeSchemasRoot + "textmate-colors.json",
-    name: "TextmateColors",
-    fname: "textmate-colors.d.ts",
-    kind: "jsonschema",
-  },
-  {
-    schema: vscodeSchemasRoot + "workbench-colors.json",
-    name: "WorkbenchColors",
-    fname: "workbench-colors.d.ts",
-    kind: "jsonschema",
-  },
+  // VSCode have started auto-generating their schema since v1.92.0 and json-schema-to-typescript isn't so happy with that.
+  // The reason for that still needs to be investigated.
+  //
+  // For now, the schemas need to be manually updated by copy pasting the schema into https://transform.tools/json-schema-to-typescript
+  // and then figure out how best to include the changes inside the `types` directory.
+  //
+  // Last known working commit: https://github.com/ota-meshi/extract-vscode-schemas/commit/93dfa59357a3181e6c5985a417ef08324ad3d388
+  // Breaking commit: https://github.com/ota-meshi/extract-vscode-schemas/commit/31417f5c16f335d220140ca775c2951fcbab32f6
+  // {
+  //   schema: vscodeSchemasRoot + "token-styling.json",
+  //   name: "SemanticTokens",
+  //   fname: "token-styling.d.ts",
+  //   kind: "jsonschema",
+  // },
+  // {
+  //   schema: vscodeSchemasRoot + "textmate-colors.json",
+  //   name: "TextmateColors",
+  //   fname: "textmate-colors.d.ts",
+  //   kind: "jsonschema",
+  // },
+  // {
+  //   schema: vscodeSchemasRoot + "workbench-colors.json",
+  //   name: "WorkbenchColors",
+  //   fname: "workbench-colors.d.ts",
+  //   kind: "jsonschema",
+  // },
   {
     schema:
-      "https://raw.githubusercontent.com/usernamehw/vscode-error-lens/v3.13.0/package.json",
+      "https://raw.githubusercontent.com/usernamehw/vscode-error-lens/v3.20.0/package.json",
     name: "ErrorLensColors",
     fname: "errorlens.d.ts",
     kind: "extension-packagejson",
   },
   {
     schema:
-      "https://raw.githubusercontent.com/gitkraken/vscode-gitlens/v14.4.0/package.json",
+      "https://raw.githubusercontent.com/gitkraken/vscode-gitlens/v16.0.1/package.json",
     name: "GitLensColors",
     fname: "gitlens.d.ts",
     kind: "extension-packagejson",
   },
   {
     schema:
-      "https://github.com/microsoft/vscode-pull-request-github/raw/v0.77.2023112709/package.json",
+      "https://github.com/microsoft/vscode-pull-request-github/raw/v0.100.0/package.json",
     name: "GitHubPullRequestColors",
     fname: "github-pull-request.d.ts",
     kind: "extension-packagejson",
